f028d0d816f576d8c5c4ed9ed39a0c55ad1c2aa3
Mads.Lee.Giil <mads.lee.giil@nav.no>
2025-11-21 09:37:40 +0100

    Test at vi returnerer nyeste deltakerAktivitet-mapping

--- src/test/kotlin/no/nav/arena_tiltak_aktivitet_acl/repositories/DeltakerAktivitetMappingRepositoryTest.kt
+++ src/test/kotlin/no/nav/arena_tiltak_aktivitet_acl/repositories/DeltakerAktivitetMappingRepositoryTest.kt
@@ -1,28 +1,49 @@
 package no.nav.arena_tiltak_aktivitet_acl.repositories
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.date.shouldHaveHour
-import io.kotest.matchers.date.shouldHaveMinute
-import io.kotest.matchers.date.shouldHaveSameDayAs
-import io.kotest.matchers.date.shouldHaveSameMonthAs
-import io.kotest.matchers.date.shouldHaveSameYearAs
+import io.kotest.matchers.shouldBe
 import no.nav.arena_tiltak_aktivitet_acl.database.SingletonPostgresContainer
+import no.nav.arena_tiltak_aktivitet_acl.domain.kafka.aktivitet.AktivitetKategori
+import no.nav.arena_tiltak_aktivitet_acl.domain.kafka.arena.tiltak.DeltakelseId
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.util.UUID
 
-class DeltakerAktivitetMappingRepositoryTest : FunSpec( {
+class DeltakerAktivitetMappingRepositoryTest : FunSpec({
+
 	val datasource = SingletonPostgresContainer.getDataSource()
-	lateinit var repository: GjennomforingRepository
-	val now = LocalDateTime.now()
+	lateinit var repository: DeltakerAktivitetMappingRespository
+
 	beforeEach {
-		repository = DeltakerAktivitetMappingRespository(NamedParameterJdbcTemplate( datasource))
+		repository = DeltakerAktivitetMappingRespository(NamedParameterJdbcTemplate(datasource))
 	}
 
-	infix fun LocalDateTime.shouldBeCloseTo(other: LocalDateTime) {
-		this shouldHaveSameYearAs other
-		this shouldHaveSameMonthAs other
-		this shouldHaveSameDayAs other
-		this shouldHaveHour other.hour
-		this shouldHaveMinute other.minute
+	test("Skal hente id for nyeste oppfølgingsperiode etter splitt") {
+		val deltakerAktivitetMappingFørSplitt = deltakerAktivitetMapping(
+			oppfølgingsperiodeId = UUID.randomUUID(),
+			oppfølgingsperiodeSlutt = ZonedDateTime.now().minusDays(1)
+		)
+		repository.insert(deltakerAktivitetMappingFørSplitt)
+		val deltakerAktivitetSomSkalFøreTilSplitt = deltakerAktivitetMapping(
+			oppfølgingsperiodeId = UUID.randomUUID(),
+			oppfølgingsperiodeSlutt = null
+		)
+		repository.insert(deltakerAktivitetSomSkalFøreTilSplitt)
+
+		val deltakerMappingEtterSplitt = repository.getCurrentDeltakerAktivitetMapping(
+			DeltakelseId(deltakerAktivitetMappingFørSplitt.deltakelseId),
+			AktivitetKategori.TILTAKSAKTIVITET
+		)!!
+
+		deltakerMappingEtterSplitt.oppfolgingsPeriodeId shouldBe deltakerAktivitetSomSkalFøreTilSplitt.oppfolgingsPeriodeId
 	}
 })
+
+private fun deltakerAktivitetMapping(oppfølgingsperiodeId: UUID, oppfølgingsperiodeSlutt: ZonedDateTime? = null) =
+	DeltakerAktivitetMappingDbo(
+		deltakelseId = 123,
+		aktivitetId = UUID.randomUUID(),
+		aktivitetKategori = "TILTAKSAKTIVITET",
+		oppfolgingsPeriodeId = oppfølgingsperiodeId,
+		oppfolgingsPeriodeSluttTidspunkt = oppfølgingsperiodeSlutt,
+	)
