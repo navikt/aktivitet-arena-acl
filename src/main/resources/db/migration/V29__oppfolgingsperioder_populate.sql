4f2d69f343295aac30f7b38b802fb1012fdf883b
sigurdgroneng <sigurd.groneng@nav.no>
2025-11-25 10:59:44 +0100

    Handter on conflict bedre

--- src/main/resources/db/migration/V29__oppfolgingsperioder_populate.sql
+++ src/main/resources/db/migration/V29__oppfolgingsperioder_populate.sql
@@ -1,10 +1,10 @@
+with oppfolgingsperioder_select as (
+    select distinct on (deltaker_aktivitet_mapping.oppfolgingsperiode_id)
+        deltaker_aktivitet_mapping.oppfolgingsperiode_id as id,
+        COALESCE(aktivitet.oppfolgingsperiode_slutt_tidspunkt , deltaker_aktivitet_mapping.oppfolgingsperiode_slutttidspunkt) as slutt
+    from deltaker_aktivitet_mapping
+        left join aktivitet on deltaker_aktivitet_mapping.oppfolgingsperiode_id = aktivitet.oppfolgingsperiode_uuid
+            and aktivitet.id = deltaker_aktivitet_mapping.aktivitetskort_id
+)
 insert into oppfolgingsperioder(id, slutt)
-select
-    deltaker_aktivitet_mapping.oppfolgingsperiode_id,
-    COALESCE(aktivitet.oppfolgingsperiode_slutt_tidspunkt , deltaker_aktivitet_mapping.oppfolgingsperiode_slutttidspunkt) as ny_slutt
-from deltaker_aktivitet_mapping
-    left join aktivitet on deltaker_aktivitet_mapping.oppfolgingsperiode_id = aktivitet.oppfolgingsperiode_uuid
-        and aktivitet.id = deltaker_aktivitet_mapping.aktivitetskort_id
-on conflict (id) do update
--- Bare oppdater slutt hvis innkommende rad har en sluttdato
-set slutt = COALESCE(excluded.slutt , oppfolgingsperioder.slutt)
+select id, slutt from oppfolgingsperioder_select;
